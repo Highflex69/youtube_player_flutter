4f97dfe600e32fa84039ec8c750d2a999ac3870f
Teddy Chavez <teddy.chavez@skandia.se>
2025-10-14 19:46:20 +0200

    refactor: Remove SingleChildScrollView from RawYoutubePlayer
    The `SingleChildScrollView` widget wrapping the `InAppWebView` has been removed. This change prevents unintended scrolling behavior within the player.

--- packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
+++ packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
@@ -82,141 +82,138 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer>
 
     return IgnorePointer(
       ignoring: true,
-      child: SingleChildScrollView(
-        child: InAppWebView(
-          key: widget.key,
-          initialData: InAppWebViewInitialData(
-            data: htmlPlayer.player,
-            encoding: 'utf-8',
-            baseUrl: WebUri.uri(Uri.https('youtube-nocookie.com')),
-            mimeType: 'text/html',
-          ),
-          initialSettings: InAppWebViewSettings(
-            userAgent: htmlPlayer.userAgent,
-            mediaPlaybackRequiresUserGesture: false,
-            transparentBackground: true,
-            disableContextMenu: true,
-            supportZoom: false,
-            disableHorizontalScroll: false,
-            disableVerticalScroll: false,
-            allowsInlineMediaPlayback: true,
-            allowsAirPlayForMediaPlayback: true,
-            allowsPictureInPictureMediaPlayback: true,
-            useWideViewPort: false,
-            useHybridComposition: controller?.flags.useHybridComposition,
-          ),
-          onWebViewCreated: (webController) {
-            controller?.updateValue(webViewController: webController);
-            webController
-              ..addJavaScriptHandler(
-                handlerName: 'Ready',
-                callback: (_) {
-                  _isPlayerReady = true;
-                  if (_onLoadStopCalled) {
-                    controller?.updateValue(isReady: true);
-                  }
-                },
-              )
-              ..addJavaScriptHandler(
-                handlerName: 'StateChange',
-                callback: (args) {
-                  PlayerState? playerState = controller?.value.playerState;
-                  bool? isPlaying = controller?.value.isPlaying;
-                  int? errorCode = controller?.value.errorCode;
-
-                  switch (args.first as int) {
-                    case -1:
-                      playerState = PlayerState.unStarted;
-                      break;
-                    case 0:
-                      if (controller?.metadata != null) {
-                        widget.onEnded?.call(controller!.metadata);
-                      }
-                      playerState = PlayerState.ended;
-                      break;
-                    case 1:
-                      playerState = PlayerState.playing;
-                      isPlaying = true;
-                      errorCode = 0;
-                      break;
-                    case 2:
-                      playerState = PlayerState.paused;
-                      isPlaying = false;
-                      break;
-                    case 3:
-                      playerState = PlayerState.buffering;
-                      break;
-                    case 5:
-                      playerState = PlayerState.cued;
-                      break;
-                    default:
-                      final e = Exception(
-                          "Invalid player state obtained: ${args.first}");
-                      debugPrint(e.toString());
-                      throw e;
-                  }
-
-                  controller?.updateValue(
-                    playerState: playerState,
-                    isLoaded: playerState != PlayerState.unStarted,
-                    isPlaying: isPlaying,
-                    hasPlayed: playerState == PlayerState.playing,
-                    errorCode: errorCode,
-                  );
-                },
-              )
-              ..addJavaScriptHandler(
-                handlerName: 'PlaybackQualityChange',
-                callback: (args) {
-                  controller?.updateValue(
-                      playbackQuality: args.first as String);
-                },
-              )
-              ..addJavaScriptHandler(
-                handlerName: 'PlaybackRateChange',
-                callback: (args) {
-                  final num rate = args.first;
-                  controller?.updateValue(
-                    playbackRate: rate.toDouble(),
-                  );
-                },
-              )
-              ..addJavaScriptHandler(
-                handlerName: 'Errors',
-                callback: (args) {
-                  final errorCode = args.first.toString();
-                  controller?.updateValue(
-                    errorCode: int.tryParse(errorCode) ?? -1,
-                  );
-                },
-              )
-              ..addJavaScriptHandler(
-                handlerName: 'VideoData',
-                callback: (args) {
-                  controller?.updateValue(
-                    metaData: YoutubeMetaData.fromRawData(args.first),
-                  );
-                },
-              )
-              ..addJavaScriptHandler(
-                handlerName: 'VideoTime',
-                callback: (args) {
-                  final position = args.first * 1000;
-                  final num buffered = args.last;
-                  controller?.updateValue(
-                    position: Duration(milliseconds: position.floor()),
-                    buffered: buffered.toDouble(),
-                  );
-                },
-              );
-          },
-          onLoadStop: (_, __) {
-            _onLoadStopCalled = true;
-            if (_isPlayerReady && controller != null) {
-              controller?.updateValue(isReady: true);
-            }
-          },
+      child: InAppWebView(
+        key: widget.key,
+        initialData: InAppWebViewInitialData(
+          data: htmlPlayer.player,
+          encoding: 'utf-8',
+          baseUrl: WebUri.uri(Uri.https('youtube-nocookie.com')),
+          mimeType: 'text/html',
         ),
+        initialSettings: InAppWebViewSettings(
+          userAgent: htmlPlayer.userAgent,
+          mediaPlaybackRequiresUserGesture: false,
+          transparentBackground: true,
+          disableContextMenu: true,
+          supportZoom: false,
+          disableHorizontalScroll: false,
+          disableVerticalScroll: false,
+          allowsInlineMediaPlayback: true,
+          allowsAirPlayForMediaPlayback: true,
+          allowsPictureInPictureMediaPlayback: true,
+          useWideViewPort: false,
+          useHybridComposition: controller?.flags.useHybridComposition,
+        ),
+        onWebViewCreated: (webController) {
+          controller?.updateValue(webViewController: webController);
+          webController
+            ..addJavaScriptHandler(
+              handlerName: 'Ready',
+              callback: (_) {
+                _isPlayerReady = true;
+                if (_onLoadStopCalled) {
+                  controller?.updateValue(isReady: true);
+                }
+              },
+            )
+            ..addJavaScriptHandler(
+              handlerName: 'StateChange',
+              callback: (args) {
+                PlayerState? playerState = controller?.value.playerState;
+                bool? isPlaying = controller?.value.isPlaying;
+                int? errorCode = controller?.value.errorCode;
+
+                switch (args.first as int) {
+                  case -1:
+                    playerState = PlayerState.unStarted;
+                    break;
+                  case 0:
+                    if (controller?.metadata != null) {
+                      widget.onEnded?.call(controller!.metadata);
+                    }
+                    playerState = PlayerState.ended;
+                    break;
+                  case 1:
+                    playerState = PlayerState.playing;
+                    isPlaying = true;
+                    errorCode = 0;
+                    break;
+                  case 2:
+                    playerState = PlayerState.paused;
+                    isPlaying = false;
+                    break;
+                  case 3:
+                    playerState = PlayerState.buffering;
+                    break;
+                  case 5:
+                    playerState = PlayerState.cued;
+                    break;
+                  default:
+                    final e = Exception(
+                        "Invalid player state obtained: ${args.first}");
+                    debugPrint(e.toString());
+                    throw e;
+                }
+
+                controller?.updateValue(
+                  playerState: playerState,
+                  isLoaded: playerState != PlayerState.unStarted,
+                  isPlaying: isPlaying,
+                  hasPlayed: playerState == PlayerState.playing,
+                  errorCode: errorCode,
+                );
+              },
+            )
+            ..addJavaScriptHandler(
+              handlerName: 'PlaybackQualityChange',
+              callback: (args) {
+                controller?.updateValue(playbackQuality: args.first as String);
+              },
+            )
+            ..addJavaScriptHandler(
+              handlerName: 'PlaybackRateChange',
+              callback: (args) {
+                final num rate = args.first;
+                controller?.updateValue(
+                  playbackRate: rate.toDouble(),
+                );
+              },
+            )
+            ..addJavaScriptHandler(
+              handlerName: 'Errors',
+              callback: (args) {
+                final errorCode = args.first.toString();
+                controller?.updateValue(
+                  errorCode: int.tryParse(errorCode) ?? -1,
+                );
+              },
+            )
+            ..addJavaScriptHandler(
+              handlerName: 'VideoData',
+              callback: (args) {
+                controller?.updateValue(
+                  metaData: YoutubeMetaData.fromRawData(args.first),
+                );
+              },
+            )
+            ..addJavaScriptHandler(
+              handlerName: 'VideoTime',
+              callback: (args) {
+                final position = args.first * 1000;
+                final num buffered = args.last;
+                controller?.updateValue(
+                  position: Duration(milliseconds: position.floor()),
+                  buffered: buffered.toDouble(),
+                );
+              },
+            );
+        },
+        onLoadStop: (_, __) {
+          _onLoadStopCalled = true;
+          if (_isPlayerReady && controller != null) {
+            controller?.updateValue(isReady: true);
+          }
+        },
       ),
     );
   }
